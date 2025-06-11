5a2a681224568099eafae8cb09eb21b8ebf652ee
auoui <oui@systemli.org>
2025-06-11 05:30:47 +0200

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Layout from '../components/Layout'
+import Quote from '../components/Quote'
 
 export default function Home() {
   return (
@@ -8,11 +9,11 @@ export default function Home() {
         <p className="text-xl font-medium">
           Selbstfindung • Held*innenreise • Workshops • Gestalttherapie i.A.
         </p>
-        <p className="italic text-lg text-gray-700">
+        <Quote>
           „Meine Heldenreise hat mir deutlich gemacht, weshalb ich in meinem Leben bisher so viele unterschiedliche Dinge lernen wollte.
           Ich habe während meiner Reise das letzte Puzzleteil gefunden, das mir noch gefehlt hatte, um alles miteinander zu verbinden.
           Die Held*innenreise ist für mich, mein inneres Strahlen in Verbindung mit anderen zu bringen und die Erfahrung, dass Verbindung auch Vertrauen bedeuten kann.“
-        </p>
+        </Quote>
         <div className="py-4">
           <a href="mailto:fine.belger@posteo.de" className="bg-pink-500 hover:bg-pink-600 text-white px-6 py-3 rounded-full shadow-lg transition-all">
             Kontakt & Anmeldung
